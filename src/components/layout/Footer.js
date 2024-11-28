b37bb17b1b7087db1adde873a7c41e3d12d09a18
mustafa <janodones@gmail.com>
2024-11-28 21:24:38 +0300

    page look  improved

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 SomalSmart IoT Solutions. All rights reserved.</p>
+        <p>&copy; 2024 HiilCore. All rights reserved.</p>
       </div>
     </footer>
   );
