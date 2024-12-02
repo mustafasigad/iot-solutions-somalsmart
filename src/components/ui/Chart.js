faf643917e8a7fba80a33883946519e8beaba6db
mustafa <janodones@gmail.com>
2024-12-02 08:24:06 +0300

    new client and supplier

--- src/components/ui/Chart.js
+++ src/components/ui/Chart.js
@@ -1,16 +1,20 @@
-// src/components/ui/Chart.js
-"use client"
+"use client";
 
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label } from 'recharts';
 
-export default function Chart({ data, dataKey }) {
+export default function Chart({ data, dataKey, xLabel = "Time", yLabel = "Value" }) {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis />
+        <XAxis dataKey="time">
+          <Label value={xLabel} offset={-5} position="insideBottom" />
+        </XAxis>
+        <YAxis>
+          <Label value={yLabel} angle={-90} position="insideLeft" />
+        </YAxis>
         <Tooltip />
+        <Legend />
         <Line type="monotone" dataKey={dataKey} stroke="#2563eb" strokeWidth={2} dot={false} />
       </LineChart>
     </ResponsiveContainer>
